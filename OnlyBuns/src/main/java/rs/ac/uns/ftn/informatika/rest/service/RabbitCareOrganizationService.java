e2fde64c63eec4836df447da94b8f6fab1afdf12
MilosNesh <neskovic.milos02@gmail.com>
2025-01-15 01:59:24 +0100

    feat: display rabbit care organizations

--- OnlyBuns/src/main/java/rs/ac/uns/ftn/informatika/rest/service/RabbitCareOrganizationService.java
+++ OnlyBuns/src/main/java/rs/ac/uns/ftn/informatika/rest/service/RabbitCareOrganizationService.java
@@ -3,7 +3,11 @@ package rs.ac.uns.ftn.informatika.rest.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import rs.ac.uns.ftn.informatika.rest.domain.Organization;
+import rs.ac.uns.ftn.informatika.rest.dto.OrganizationDTO;
 import rs.ac.uns.ftn.informatika.rest.repository.IRabbitCareOrganizationRepository;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class RabbitCareOrganizationService {
@@ -12,5 +16,14 @@ public class RabbitCareOrganizationService {
 
     public void save(Organization organization) {
         repository.save(organization);
+    }
+
+    public List<OrganizationDTO> findAll() {
+        List<Organization> organizations = repository.findAll();
+        List<OrganizationDTO> dtos = new ArrayList<>();
+        for (Organization organization : organizations) {
+            dtos.add(new OrganizationDTO(organization));
+        }
+        return dtos;
     }
 }
